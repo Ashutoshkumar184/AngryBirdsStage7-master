728a297245a9413f3b001f3d86f063a4d702d07a
Ashutoshkumar184 <70849250+Ashutoshkumar184@users.noreply.github.com>
2021-05-20 12:36:19 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,22 @@
+class Ball{
+    constructor(x, y, width, height, angle) {
+        var options = {
+            'frictionAir':0.05,
+            'density':1,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+      
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        
+      ellipse(0,0,this.width,this.height)
+        pop();
+      }
+}
